a0833e90047e016afe09cb91eed63bc9a0067ae2
1tsAb <abubakary.conteh24@gmail.com>
2025-03-03 17:52:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let fruits = { "Grapes", "Apples", "Pears", "Oranges"};
+let fruit = fruit[0]; //first fruit
 
+function logFirstFruit()
+{
+  let randomNumber= Math.floor(Math.random()*fruits.length);
+  console.log(fruits[);
+}
+logFirstFruit();
